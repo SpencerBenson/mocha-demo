d7cdc3c908af4d92a8a8beb616ea395a7a6cead3
Ravi Kumar <Ravi.Kumar@arm.com>
2022-11-11 08:58:43 +0300

    create an fn to call add method + fn with callback

--- specs/myClass.spec.js
+++ specs/myClass.spec.js
@@ -1,10 +1,27 @@
 import  MyClass  from '../src/myClass.js'
 import {expect} from 'chai'
 const myObj = new MyClass()
+import sinon from 'sinon'
 
 
 describe("Test suite for MyClass", function () {
   it("Test the add method", function () { 
       expect (myObj.add(1,2)).to.equal(3)
   })
+  it("Spy the add method", function () { 
+    const spy = sinon.spy(myObj, 'add')
+    const arg1 = 10
+    const arg2 = 20
+    myObj.callAnotherFn(arg1, arg2)
+    // sinon.assert.calledOnce(spy)
+    expect(spy.calledOnce).to.be.true
+    expect(spy.calledOnceWith(arg1, arg2)).to.be.true
+  })
+
+  it('should call the callback fn', function () {
+    let callbackspy = sinon.spy()
+    myObj.callTheCallback(callbackspy)
+    expect(callbackspy.calledOnce).to.be.true
+    
+})
 })
--- src/myClass.js
+++ src/myClass.js
@@ -2,9 +2,20 @@ class MyClass{
   constructor() {
       console.log("Initiate")
   }
+
   add(arg1, arg2) {
     return arg1 +arg2
   }
+
+  callAnotherFn(arg1, arg2) {
+    return this.add(arg1, arg2)
+  }
+
+  callTheCallback(callback) {
+    callback()
+  }
 }
+
+
 
 export default MyClass
